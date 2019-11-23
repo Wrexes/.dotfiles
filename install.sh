19bd931fa645e07ade28d78e28d0002f65d03291
Renard <ludovic.fernandez@epitech.eu>
2019-11-24 00:10:39 +0100

    Add basic install script (no dependency check)

---
+++ install.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+#
+# install.sh
+# Copyright (C) 2019 renard <renard@voyager>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+CWD=$(pwd)
+
+git submodule update --init --recursive
+
+cd $HOME
+    && ln -s $CWD/clang-format/.clang-format .
+    && ln -s $CWD/.vimrc .vimrc
+    && ln -s $CWD/.gitconfig .
+    && ln -s $CWD/gdb-dashboard/.gdbinit .
+
+mkdir -p "$HOME/.config" "HOME/.local"
+cd $HOME/.config
+    && ln -s $CWD/mimeapps.list .
+    && ln -s $CWD/mimeinfo.cache .
+    && ln -s $CWD/starship.toml .
+    && ln -s $CWD/yay .
+    && ln -s $CWD/i3 .
+    && ln -s $CWD/fish .
+    && ln -s $CWD/rofi .
+    && ln -s $CWD/omf .
+    && ln -s $CWD/kitty .
+    && ln -s $CWD/gdb .
