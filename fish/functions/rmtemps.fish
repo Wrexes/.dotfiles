f6f1433dbc7d39dfa3f237cbf2b8cb93541281a1
Renard <ludovic.fernandez@epitech.eu>
2020-01-11 23:38:15 +0100

    [FISH] Update rmtemps
    Sort name templates
    Add some new ones

--- fish/functions/rmtemps.fish
+++ fish/functions/rmtemps.fish
@@ -1,19 +1,23 @@
-# Defined in /tmp/fish.NSLHaA/rmtemps.fish @ line 2
+# Defined in /tmp/fish.G1kgl0/rmtemps.fish @ line 2
 function rmtemps --description 'Remove temp files recursively'
 	find . -depth                   \
         -type f                     \
         \(                          \
             -name "#*#"             \
-            -o -name "*~"           \
-            -o -name "*.o"          \
             -o -name "*.d"          \
-            -o -name "*.gcno"       \
             -o -name "*.gcda"       \
-            -o -name "vgcore.*"     \
-            -o -name ".gdb_history" \
-            -o -name "a.out"        \
-            -o -name ".#*"          \
+            -o -name "*.gcno"       \
+            -o -name "*.gcov"       \
+            -o -name "*.o"          \
             -o -name "*.orig"       \
+            -o -name "*.tmp"        \
+            -o -name "*~"           \
+            -o -name ".#*"          \
+            -o -name ".bash_history"\
+            -o -name ".gdb_history" \
+            -o -name ".nvimlog"     \
+            -o -name "a.out"        \
+            -o -name "vgcore.*"     \
         \)                          \
         -print                      \
         -delete
