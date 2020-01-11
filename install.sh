cfedf372382d7e3ee30ef277af1133319799d826
Renard <ludovic.fernandez@epitech.eu>
2020-01-11 23:22:37 +0100

    [INSTALL] Fix missing $ for ~/.local creation, sort instructions

--- install.sh
+++ install.sh
@@ -11,19 +11,19 @@ CWD=$(pwd)
 
 git submodule update --init --recursive
 
-ln -s $CWD/clang-format/.clang-format   $HOME/
-ln -s $CWD/.vim*                        $HOME/
-ln -s $CWD/.gitconfig                   $HOME/
-ln -s $CWD/gdb-dashboard/.gdbinit       $HOME/
 ln -s $CWD/.gdbinit.d/                  $HOME/
+ln -s $CWD/.gitconfig                   $HOME/
+ln -s $CWD/.vim*                        $HOME/
+ln -s $CWD/clang-format/.clang-format   $HOME/
+ln -s $CWD/gdb-dashboard/.gdbinit       $HOME/
 
-mkdir -p "$HOME/.config" "HOME/.local"
+mkdir -p "$HOME/.config" "$HOME/.local"
+ln -s $CWD/fish             $HOME/.config/
+ln -s $CWD/i3               $HOME/.config/
+ln -s $CWD/kitty            $HOME/.config/
 ln -s $CWD/mimeapps.list    $HOME/.config/
 ln -s $CWD/mimeinfo.cache   $HOME/.config/
+ln -s $CWD/omf              $HOME/.config/
+ln -s $CWD/rofi             $HOME/.config/
 ln -s $CWD/starship.toml    $HOME/.config/
 ln -s $CWD/yay              $HOME/.config/
-ln -s $CWD/i3               $HOME/.config/
-ln -s $CWD/fish             $HOME/.config/
-ln -s $CWD/rofi             $HOME/.config/
-ln -s $CWD/omf              $HOME/.config/
-ln -s $CWD/kitty            $HOME/.config/
