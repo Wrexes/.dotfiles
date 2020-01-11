40128e641e8ad54d079ba8a8ab1432b4da955f99
Renard <ludovic.fernandez@epitech.eu>
2020-01-11 23:39:53 +0100

    [FISH] Update update-all
    Add --noconfirm to yay cleaning commands
    Make yay cleaning commands follow each other with an 'and'
    Add updating NeoVim plugins and CoC extensions

--- fish/functions/update-all.fish
+++ fish/functions/update-all.fish
@@ -1,12 +1,16 @@
-# Defined in /tmp/fish.zY5HdU/update-all.fish @ line 2
+# Defined in /tmp/fish.2r3Mp3/update-all.fish @ line 2
 function update-all --description 'Update everything with a single command.'
-    figlet -tf slant "YAY "
+	figlet -tf slant "YAY "
 	figlet -tf small "Cleaning cache..."
-    yay -Yc
-    yay -Sc
+    yay -Yc --noconfirm
+    and yay -Sc --noconfirm
     figlet -tf small "Updating packages..."
     yay
     echo; echo;
+
+    figlet -tf slant "NeoVim"
+    nvim +PlugUpdate +CocUpdate +qa
+    figlet -tf small "Done."
 
     figlet -tf slant "Oh My Fish !"
     omf update
