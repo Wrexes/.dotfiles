1e79e7d2287f7773d9d2a1e6534555716da04dd2
Renard <ludovic.fernandez@epitech.eu>
2020-04-03 15:31:41 +0200

    [FISH] Add setting up Perl's Local::Lib

--- fish/config.fish
+++ fish/config.fish
@@ -17,8 +17,14 @@
     bind \el 'git plog; echo; commandline -f repaint'
     # Alt+s : Print a compact git status status
     bind \es 'state; echo; commandline -f repaint'
+    # Alt+g : Show a git diff
+    bind \eg 'git diff; echo; commandline -f repaint'
 
 
+# Perl Local::Lib
+if command -v perl > /dev/null 2>&1 && command -v cpanm > /dev/null 2>&1
+  test -d "$HOME/perl5/lib/perl5" && eval (perl -I "$HOME/perl5/lib/perl5" -Mlocal::lib)
+end
 
-# For setting $PATH, check <fish_conf_dir>/conf.d/path.fish
-# For other environment variables, see <fish_conf_dir>/conf.d/env.fish
+# For setting $PATH, check $fish_config/conf.d/path.fish
+# For other environment variables, see $fish_config/conf.d/env.fish
